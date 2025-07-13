800ef2997df9a72664a35fc7718c2888bdd8f956
Scorpgodwest <scorpgodwest@gmail.com>
2025-07-13 16:15:00 +0300

    Initial Commit

---
+++ swiftuicourse/swiftuicourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftuicourseApp.swift
+//  swiftuicourse
+//
+//  Created by Мирон Лабус on 13.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct swiftuicourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
